c4afb3da6b4de995acc9a64d4c27ed05ad988f50
Weit145 <weit.dima@yandex.ru>
2025-12-07 16:43:20 +0300

    Add init.py for post :alien:

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 __all__ = ["auth_pb2", "auth_pb2_grpc",
             "user_pb2", "user_pb2_grpc",
+            "post_pb2", "post_pb2_grpc",
     ]
 
 from auth import auth_pb2
@@ -7,3 +8,6 @@ from auth import auth_pb2_grpc
 
 from user import user_pb2
 from user import user_pb2_grpc
+
+from post import post_pb2
+from post import post_pb2_grpc

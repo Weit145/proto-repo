ec3419518afa05c9ebded405e4b75578fc4d0914
Weit145 <weit.dima@yandex.ru>
2025-12-08 13:38:54 +0300

    Update init :memo:

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 __all__ = ["auth_pb2", "auth_pb2_grpc",
             "user_pb2", "user_pb2_grpc",
             "post_pb2", "post_pb2_grpc",
+            "user_post_pb2", "user_post_pb2_grpc"
     ]
 
 from auth import auth_pb2
@@ -11,3 +12,6 @@ from user import user_pb2_grpc
 
 from post import post_pb2
 from post import post_pb2_grpc
+
+from user_post import user_post_pb2
+from user_post import user_post_pb2_grpc

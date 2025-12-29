1070b7c029a7dcd0ff0b480c9fa0de25d3ba95d7
Weit145 <weit.dima@yandex.ru>
2025-12-29 14:17:04 +0300

    /

--- __init__.py
+++ __init__.py
@@ -7,6 +7,8 @@ __all__ = [
     "post_pb2_grpc",
     "user_post_pb2",
     "user_post_pb2_grpc",
+    "admin_pb2",
+    "admin_pb2_grpc",
 ]
 
 from auth import auth_pb2
@@ -20,3 +22,6 @@ from post import post_pb2_grpc
 
 from user_post import user_post_pb2
 from user_post import user_post_pb2_grpc
+
+from admin import admin_pb2
+from admin import admin_pb2_grpc
